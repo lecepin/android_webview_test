4f743121f58efcac6b6363ad665fb201c62db822
lecepin <383810086@qq.com>
2023-07-13 22:38:58 +0800

    feat: js invoke native

--- app/src/main/java/com/ileping/shell/SplashActivity.kt
+++ app/src/main/java/com/ileping/shell/SplashActivity.kt
@@ -1,9 +1,11 @@
 package com.ileping.shell
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.webkit.JsPromptResult
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -14,11 +16,14 @@ import java.lang.Thread.sleep
 
 class SplashActivity : AppCompatActivity() {
     lateinit var webView: WebView
+    lateinit var container: LinearLayout
+    var threadBootMain = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
+        container = findViewById<LinearLayout>(R.id.splash_container)
 
         webView = WebView(this).apply {
             settings.apply {
@@ -27,12 +32,35 @@ class SplashActivity : AppCompatActivity() {
                 blockNetworkImage = false
             }
             webViewClient = WebViewClient()
-            webChromeClient = WebChromeClient()
+            webChromeClient = object : WebChromeClient() {
+                override fun onJsPrompt(
+                    view: WebView?,
+                    url: String?,
+                    message: String?,
+                    defaultValue: String?,
+                    result: JsPromptResult?
+                ): Boolean {
+                    val uri = Uri.parse(message)
+                    if (url != null && message != null) {
+                        if (message.startsWith("native://")) {
+
+                            when (uri.host) {
+                                "skipSplash" -> {
+                                    goHome()
+                                    result?.confirm("call natvie api success")
+                                }
+                            }
+                            return true
+                        }
+                    }
+                    return super.onJsPrompt(view, url, message, defaultValue, result)
+                }
+            }
 
             loadUrl("file:///android_asset/splash.html")
         }
 
-        findViewById<LinearLayout>(R.id.splash_container).addView(
+        container.addView(
             webView,
             LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
@@ -42,12 +70,20 @@ class SplashActivity : AppCompatActivity() {
 
         Thread {
             try {
-                sleep(3000)
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
+                sleep(5000)
+                goHome()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         }.start()
+    }
+
+    fun goHome() {
+        if (threadBootMain) {
+            return
+        }
+        threadBootMain = true
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
